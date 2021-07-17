cbbce60e0bcd1b211affa57a4e608c8bcd16abf9
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-17 00:51:02 -0400

    updating manifest.json and service worker file

---
+++ public/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install event handler
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then( cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/index.js',
+          '/icons/icon-192x192.png',
+          '/icons/icon-512x512.png',
+          '/assets/images/map.jpg',
+          '/manifest.json',
+        ]);
+      })
+    );
+    console.log('Install');
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+});
+  
+  
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then( response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
