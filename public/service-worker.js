ed2d58ed003881b0bffee1eb156739011a9ddd08
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-17 14:14:24 -0400

    Fixed the manifest file errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,30 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install event handler
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
     event.waitUntil(
-      caches.open(CACHE_NAME).then( cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/index.js',
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png',
-          '/assets/images/map.jpg',
-          '/manifest.json',
-        ]);
-      })
+        caches.open(CACHE_NAME).then( cache => {
+          console.log("Your files were pre-cached successfully!");
+          return cache.addAll(FILES_TO_CACHE);
+       })
     );
-    console.log('Install');
+
     self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    evt.waitUntil(
+    event.waitUntil(
       caches.keys().then(keyList => {
         return Promise.all(
           keyList.map(key => {
@@ -41,11 +42,27 @@ self.addEventListener("activate", event => {
   
   
 // retrieve assets from cache
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
+    if(event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
     event.respondWith(
       caches.match(event.request).then( response => {
         return response || fetch(event.request);
       })
     );
-  });
-  
+});
